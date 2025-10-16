414e4d956c070269207024f8c4fbe16c327ec0f0
paulose610 <paulose610@gmail.com>
2025-10-16 14:52:16 +0000

    old model, new folder structure

---
+++ scripts/eval.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+from joblib import load
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Paths
+script_dir = os.path.dirname(os.path.abspath(__file__))
+print(script_dir)
+data_path = os.path.join(script_dir, "../data")
+model_path = os.path.join(script_dir, "../model.joblib")
+eval_path = os.path.join(script_dir, "../metrics.csv")
+
+# Load model
+model = load(model_path)
+
+# Load test data
+test_data = pd.read_csv(os.path.join(data_path, "validate.csv")) 
+y_test = test_data[['species']].values.ravel()
+X_test = test_data.drop('species',axis=1)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Compute metrics
+metrics = {
+    "accuracy": accuracy_score(y_test, y_pred),
+    "precision": precision_score(y_test, y_pred, average="weighted"),
+    "recall": recall_score(y_test, y_pred, average="weighted"),
+    "f1_score": f1_score(y_test, y_pred, average="weighted")
+}
+
+# Save metrics to CSV
+pd.DataFrame([metrics]).to_csv(eval_path, index=False)
---
+++ scripts/train.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+from datetime import datetime
+import os
+
+# Load the data
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(script_dir, "../data")
+X_train = pd.read_csv(os.path.join(data_path, "X_train.csv"))
+X_test = pd.read_csv(os.path.join(data_path, "X_test.csv"))
+y_train = pd.read_csv(os.path.join(data_path, "y_train.csv")).values.ravel()  # flatten
+y_test = pd.read_csv(os.path.join(data_path, "y_test.csv")).values.ravel()
+
+# Initialize the Decision Tree classifier with some simple hyperparameters
+clf = DecisionTreeClassifier(max_depth=1, min_samples_split=10, random_state=42)
+
+# Train the model
+clf.fit(X_train, y_train)
+
+# Evaluate
+y_pred = clf.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Accuracy on train set: {acc:.4f}")
+
+# Save the model using joblib
+model_file = "model.joblib"
+joblib.dump(clf, model_file)
+
+# Print timestamp
+print(f"Model saved as '{model_file}' at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
